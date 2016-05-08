c10b42c6a69dee4380d5853a6ba14b56687fa012
CenoX <CenoX@CenoXui-MacBook-Pro.local>
2016-05-09 00:07:53 +0900

    Initial Commit

---
+++ SDVX Battery/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SDVX Battery
+//
+//  Created by CenoX on 2016. 5. 9..
+//  Copyright © 2016년 CenoX. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
